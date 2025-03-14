627630019a5e6ff322bd268549f921220268284d
venticx <venticx@gmail.com>
2025-03-14 11:36:56 +0100

    implemented first textview to display content of http get request body

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"io"
+	"net/http"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	app := tview.NewApplication()
+	textView := tview.NewTextView().
+			SetWordWrap(true).
+			SetChangedFunc(func() {
+				app.Draw()
+			})
+
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	textView.SetText(string(content))
+
+	if err := app.SetRoot(textView, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
